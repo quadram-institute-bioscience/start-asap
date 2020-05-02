7d4f60f91006216b9a3f2d90d065de43d0edc9ac
Andrea Telatin <andrea@telatin.com>
2020-05-02 09:28:47 +0100

    bioconda recipe: final release

--- bioconda/build.sh
+++ bioconda/build.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
- 
 mkdir -p ${PREFIX}/bin
 chmod +x ./start-asap
+#mv ./asap-lib    ${PREFIX}/bin/
 mv ./start-asap  ${PREFIX}/bin/
-#mv ./start-asap ./asap-lib ${PREFIX}/bin/
-
-
-mv ./start-asap ${PREFIX}/bin/
